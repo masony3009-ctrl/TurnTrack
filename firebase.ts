b27ecabe71cf62397cee1a6a627a75c9330e2af8
Mason Young <masony3009@gmail.com>
2026-05-07 19:18:31 -0700

    use env vars for firebase config

--- firebase.ts
+++ firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBpSJEixry1qJBWrsvS_Azlf40GWnOGCcE",
-  authDomain: "cleanerapp-3f196.firebaseapp.com",
-  projectId: "cleanerapp-3f196",
-  storageBucket: "cleanerapp-3f196.firebasestorage.app",
-  messagingSenderId: "396461166521",
-  appId: "1:396461166521:web:a5acb7041d796a2da87068"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
